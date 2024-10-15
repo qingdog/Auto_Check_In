0c406f5403a0500ae7b0f2998eab5d791980ca84
qingdog <qingdoor@gmail.com>
2024-10-15 08:50:47 +0800

    测试goto页面加载问题-登录后延迟等待

--- pig_chrome_login.py
+++ pig_chrome_login.py
@@ -66,8 +66,6 @@ async def chrome_init():
     # 全局设置导航超时时间为 60 秒
     page1.setDefaultNavigationTimeout(60000 * 3)
 
-
-
     if page1 is None:
         page1 = await chrome_browser.newPage()
     # page=page
@@ -94,7 +92,7 @@ async def login(username, password, url):
         else:
             raise Exception('无法找到登录按钮')
 
-        # 等待1s再打开页面
+        # 登录完成后等待1s再打开页面
         delay = random.randint(2000, 3000)
         await delay_time(delay)
 
@@ -105,7 +103,6 @@ async def login(username, password, url):
         else:
             print('无法找到签到按钮')
         # await page1.waitForNavigation()
-
 
         is_logged_in = await page1.evaluate('''() => {
             const logoutButton = document.querySelector('a[href="/user/logout"]');
