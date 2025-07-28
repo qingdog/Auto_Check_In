e2b8461c20651a02033ae4fef780bf74ffb98ae6
qingdog <69775024+qingdog@users.noreply.github.com>
2025-07-28 13:52:40 +0800

    Update ikuuu2.py

--- ikuuu2.py
+++ ikuuu2.py
@@ -7,7 +7,7 @@
 # cron "30 5 * * *" script-path=xxx.py,tag=匹配cron用
 # const $ = new Env('IKuuu机场签到帐号版')
 
-import requests, re
+'''import requests, re
 
 import requests
 import os
@@ -20,7 +20,7 @@ import os
 def main():
     r = 1
     oy = ql_env()
-    print("ikuuu共找到" + str(len(oy)) + "个账号")
+    print("共找到" + str(len(oy)) + "个账号")
     for i in oy:
         print("------------正在执行第" + str(r) + "个账号----------------")
         email = i.split('&')[0]
@@ -35,8 +35,7 @@ def sign_in(email, passwd):
         headers = {
             'user-agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, '
                           'like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1'}
-        # ikuuu.club
-        res = requests.get('https://ikuuu.one/', headers=headers) # ikuuu.de ikuuu.top ikuuu.club
+        res = requests.get('https://ikuuu.club/', headers=headers)
         url = re.findall('target="_blank">(.*?)</a>', res.text, re.S)
         for i in range(len(url)):
             resp = requests.session()
@@ -44,11 +43,11 @@ def sign_in(email, passwd):
             ss = resp.post(f'{url[i]}user/checkin').json()
             #         print(ss)
             if 'msg' in ss:
-                print("ikuuu.top " + ss['msg'])
+                print("ikuuu.club " + ss['msg'])
                 # notify.send("IKuuu机场签到", ss['msg'])
                 break
     except:
-        print('ikuuu.top 请检查帐号配置是否错误')
+        print('ikuuu.club 请检查帐号配置是否错误')
         # notify.send("IKuuu机场签到", '请检查帐号配置是否错误')
 
 
@@ -68,4 +67,48 @@ def ql_env():
 
 
 if __name__ == '__main__':
-    main()
+    main()'''
+    
+#document.querySelectorAll("#swal2-content")[0].textContent
+import re
+from playwright.sync_api import Playwright, sync_playwright, expect
+
+
+def run(playwright: Playwright) -> None:
+    #browser = playwright.chromium.launch(headless=False)
+    from find_chrome_util import find_chrome_util
+    import platform
+    import os
+    from dotenv import load_dotenv
+    load_dotenv()
+    browser = playwright.chromium.launch(headless=platform.system() != "Windows", executable_path=find_chrome_util())
+    context = browser.new_context()
+    page = context.new_page()
+    page.goto("https://ikuuu.club/")
+    with page.expect_popup() as page1_info:
+        print(page.locator("a").nth(0).inner_html())
+        page.locator("a").nth(0).click()
+        #page.get_by_role("link", name="https://ikuuu.one/").click()
+        #page.get_by_role("a").nth(0).click()  # 使用 .nth(1) 获取第二个按钮
+        #page.locator("a").click()
+    page1 = page1_info.value
+    page1.get_by_role("textbox", name="邮箱").click()
+    page1.get_by_role("textbox", name="邮箱").fill(os.getenv("IKUUU_USERNAME"))
+    page1.get_by_role("textbox", name="密码").click()
+    page1.get_by_role("textbox", name="密码").fill(os.getenv("IKUUU_PASSWORD"))
+    page1.get_by_role("button", name="登录", exact=True).click()
+    page1.get_by_role("button", name="OK").click()
+    page1.get_by_role("button", name="Read").click()
+    page1.get_by_role("link", name=" 每日签到").click()
+    expect(page1.locator("#swal2-title")).to_contain_text("签到成功")
+    
+    print(page.query_selector("#swal2-content").text_content())
+
+    # ---------------------
+    context.close()
+    browser.close()
+
+
+with sync_playwright() as playwright:
+    run(playwright)
+
